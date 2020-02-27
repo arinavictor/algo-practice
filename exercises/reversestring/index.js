a7add7c1f84c5c850cb71d31e6d8dd44489f76b6
arinavictor <arinavictor@yahoo.com>
2020-02-27 14:14:29 -0700

    algorithm practice questions first commit

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   const arr = str.split("");
+//   arr.reverse();
+//   return arr.join("");
+// }
+
+// function reverse(str) {
+//   return str.split("").reduce((rev, char) => char + rev, "");
+// }
+
+function reverse(str) {
+  let reversed = "";
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
 
 module.exports = reverse;
